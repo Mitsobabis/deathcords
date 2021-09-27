4bbb89af68ae2228a376ef6df280ec7b5c7d88a2
Mitsobabis <91488150+Mitsobabis@users.noreply.github.com>
2021-09-27 19:29:11 +0300

    Add files via upload

---
+++ DeathCoordinates/src/Main.java
@@ -0,0 +1,11 @@
+import listeners.DeathListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Beer Toggled");
+        Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
+    }
+}
